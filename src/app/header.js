bfbe98eb20239a0b97814bc8375a4c116c83ccf8
Ava <ava.soyeon.kim@gmail.com>
2021-12-12 22:09:58 +0900

    add journal link on header

--- src/app/header.js
+++ src/app/header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import StyledHeader from '../styles/headerStyle';
 
 const Header = () => {
   return (
     <StyledHeader>
-      <h1>HEADER</h1>
+      <Link to='/'>Journal</Link>
     </StyledHeader>
   );
 };
--- src/styles/headerStyle.js
+++ src/styles/headerStyle.js
@@ -2,13 +2,18 @@ import styled, { css } from 'styled-components';
 
 const StyledHeader = styled.div`
   ${({ theme }) => {
-    const { paddings, border } = theme;
+    const { paddings, border, fonts } = theme;
     return css`
       padding: ${paddings.large};
       display: flex;
-      justify-content: center;
+      justify-content: flex-end;
       height: 10vh;
+      font-size: ${fonts.size.regular};
       border-bottom: ${border.default};
+
+      a {
+        padding-right: ${paddings.large};
+      }
     `;
   }}
 `;
